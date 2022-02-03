d861608a5535a99bcfb092bf5094401db5971797
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-03 07:39:39 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,5 @@ int main(void)
 	printf("Size of long int: %2d byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %2d byte(s)\n", sizeof(long long int));
 	printf("Size of float: %2d byte(s)\n", sizeof(float));
-	getch();
 	return (0);
 }
