1e0968b73675abc0aef503d581df8bb02d33cb49
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-04 09:53:22 -0800

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9' ; ++n)
+{
+	putchar(n);
+}
+	putchar('\n');
+	return (0);
+}
