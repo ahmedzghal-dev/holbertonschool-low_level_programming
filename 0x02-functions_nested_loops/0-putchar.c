a198bf2700bea46190a03b3ee2ab69a49163b0bc
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-08 09:15:18 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int main (void)
 {
 	int i;
 	char *str;
 	str = "_putchar\n";
-	for(i = 0 ; i <= 8; i++)
+	for (i = 0 ; i <= 8; i++)
 	{
 	_putchar(str[i]);
 	}
