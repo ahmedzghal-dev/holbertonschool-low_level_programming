5cd2d96e3a5428bbcf4b37e59932b9fe2dd6071b
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-08 09:49:49 -0800

    _putcher

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - write check the code
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *Return: Always 0.
  */
 int main(void)
 {
