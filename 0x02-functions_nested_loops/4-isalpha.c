6ae74f29d447342b06dfcaba085ded4bcb218aba
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-08 13:08:06 -0800

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,12 @@
  * _isalpha - alphabetic char checker
  * @c: c is one character
  *
- *Returns 1 if c is a letter, lowercase or uppercase otherwise 0
+ *Return: 1 if c is a letter lowercase or uppercase.else returns 0
  */
 int _isalpha(int c)
 
 {
-	if ((c >= 97 && c <= 122) || (c >=65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	return (1);
 	else
 	return (0);
