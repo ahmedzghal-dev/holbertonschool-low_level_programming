14442b489ba789fffbd0d8c33b2322119b24d41c
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-08 14:43:49 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - check the code
+ *@n: n is integer
+ *
+ * Return: in n is negative print result, print n if it is positive or 0
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+{
+	n = n * -1;
+	return (n);
+	}
+	else
+	{
+	return (n);
+}
+}
+
