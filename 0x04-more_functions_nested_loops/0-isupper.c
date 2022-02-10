8475ba9b228af8fbdfd41142bf734a8ab5005feb
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-10 01:50:06 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - alphabet checker.
+ * @c: integer
+ *
+ * Return: if c uppercase print 1, else print 0.
+ */
+	int _isupper(int c)
+{
+	int c = 'c';
+
+	if (isupper(c))
+{
+	_putchar(0);
+	_putchar("\n");
+}
+	else
+	return (1);
+}
