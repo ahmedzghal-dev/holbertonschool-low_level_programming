db1325b0026b7696207f9f3b9802ad188556d2d0
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-10 07:52:11 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,13 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (j = 0; j <= 9; j++)
+	for (j = 0; j < 10; j++)
 {
 	for (i = 0; i <= 14; i++)
 {
-	_putchar(i);
+	if ( i > 9)
+	_putchar((i / 10) + 48);
+	_putchar((i % 10) + 48);
 }
 	_putchar('\n');
 }
