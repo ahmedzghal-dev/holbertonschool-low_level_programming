80771fd62eb4747f81bad4ce44b6d004443e42ad
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-21 10:20:15 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strchr - check the code
+ *@s: pointer
+ *@c: character
+ *
+ * Return: always s
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; ++s)
+	{
+		if (*s == c)
+		break;
+	}
+
+	return (s);
+}
