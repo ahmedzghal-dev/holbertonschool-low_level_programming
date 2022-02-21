ba427424a74fe4874f33b4ab742fdef3c4b4f608
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-21 08:19:41 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * *_memset - check the code
+ *_memset - check the code
+ *@s: pointer
+ *@b: character
+ *@n: insigned integer
  *
+ * Return: always s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *i = s;
 
-	while(n--)
+	while (n--)
 	{
 		*i++ = b;
 	}
