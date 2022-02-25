77093911d88a95a2b9565b23bfd366f70e701af8
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-02-25 15:34:43 +0000

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *main: check the code
+ *main - check the code
  *@argc:argument counter
  *@argv:argument value
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
 	if (argc)
     printf("%s\n", argv[0]);
