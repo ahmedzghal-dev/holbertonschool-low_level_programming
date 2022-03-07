cbb7c69049bf93ec98adef8bbf2d952820ab1ed1
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-03-07 06:49:02 -0800

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef function_like_macro.h
-#define function_like_macro.h
+#ifndef function_like_macro_h
+#define function_like_macro_h
 
-#define ABS(X)  (((X) < (0)) ? -(X) : (X))
+#define ABS(X)  (((x) < (0)) ? -(x) : (x))
 
 #endif
