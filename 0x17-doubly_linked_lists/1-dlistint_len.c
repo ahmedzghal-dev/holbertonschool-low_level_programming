375edfa02bec071fcc7efab7f3277a4498d172b4
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-05 11:01:28 +0000

    dlistint_len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - returns the length of the list
+ * @h: pointer to the list
+ *
+ * Return: count list length
+ */
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - returns the length of the list
+ * @h: pointer to the list
+ *
+ * Return: count list length
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t cnt = 0;
+
+	while (h != NULL)
+	{
+		cnt++;
+	h = h->next;
+	}
+	return (cnt);
+}
