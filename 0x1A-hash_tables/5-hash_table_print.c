a62d2ba59a8b09204584c5f87cbbe3d057c07b6e
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-27 20:29:03 +0000

    print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,24 +6,24 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned int index = 0, b = 0;
+unsigned int index = 0, b = 0;
 
-    if (ht == NULL)
-    return;
+if (ht == NULL)
+return;
 
-    printf("{");
-    while (index < ht->size)
-    {
-        while (ht->array[index])
-        {
-            if(b == 0)
-            b = 1;
-            else
-            printf(", ")
-            printf("'%s': '%s'",  ht->array[index]->key, ht->array[index]->value);
-			ht->array[index] = ht->array[index]->next;
-        }
-        index++;
-    }
-    printf("{\n");
+printf("{");
+while (index < ht->size)
+{
+while (ht->array[index])
+{
+if (b == 0)
+b = 1;
+else
+printf(", ")
+printf("'%s': '%s'",  ht->array[index]->key, ht->array[index]->value);
+ht->array[index] = ht->array[index]->next;
+}
+index++;
+}
+printf("{\n");
 }
