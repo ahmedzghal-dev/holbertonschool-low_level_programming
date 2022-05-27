8797370fc83a266a120cd38833d6f46bd2039da9
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-27 20:13:08 +0000

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - check the code
+ *@ht: the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned int index = 0, b = 0;
+
+    if (ht == NULL)
+    return;
+
+    printf("{");
+    while (index < ht->size)
+    {
+        while (ht->array[index])
+        {
+            if(b == 0)
+            b = 1;
+            else
+            printf(", ")
+            printf("'%s': '%s'",  ht->array[index]->key, ht->array[index]->value);
+			ht->array[index] = ht->array[index]->next;
+        }
+        index++;
+    }
+    printf("{\n");
+}
