5a352633b7d615549ed07422e5c4dde933c8d0b4
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-27 20:25:55 +0000

    hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,17 +12,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 unsigned long int i = 0;
 hash_table_t *ht = malloc(sizeof(hash_table_t) * 1);
 
-if(ht == NULL)
+if (ht == NULL)
 {
-    free(ht);
-    return(NULL);
+free(ht);
+return (NULL);
 }
 ht->array = malloc(sizeof(hash_node_t *) * size);
 
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-    ht->array[i] = NULL;
+ht->array[i] = NULL;
 }
 ht->size = size;
-return(ht);
+return (ht);
 }
